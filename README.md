0add10e6d18b93888d83f6752f098f64da22dd03
Young <iamlovedit@163.com>
2023-11-14 10:02:54 +0800

    refactor project

--- README.md
+++
@@ -1,5 +0,0 @@
-# FamilyLibrary.Web
-
-a management system for family library base on React.js
-
-Thanks for ![JetBrains Logo (Main) logo](https://resources.jetbrains.com/storage/products/company/brand/logos/jb_beam.png)
--- src/models/FamilyCategory.ts
+++
@@ -1,8 +0,0 @@
-//https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-7.html
-
-export class FamilyCategory{
-    name:string | undefined;
-    code:string|undefined;
-    parent:FamilyCategory|undefined;
-    children:FamilyCategory[]|undefined
-}
